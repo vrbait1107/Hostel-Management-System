6e86a2e7b67b02f275c25f98448b813727c79a33
Vishal_Bait <vishalbait01@gmail.com>
2020-10-08 20:06:39 +0530

    Change UI of faqs, add option for navbar, rename login and logout.
    1. Delete project_register, We have already one register form we can use for logi purpose.
    2. Rename two files for simplicity.

--- includes/navbar.php
+++ includes/navbar.php
@@ -43,9 +43,22 @@
        <li class="nav-item">
         <a class="nav-link" href="faqs.php">FAQs</a>
       </li>
-
-
     </ul>
+
+
+     <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"><i
+                        class="fa fa-user fa-2x"></i></a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#">My Profile</a>
+                    <a class="dropdown-item" href="#">Account</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" id="userLogout" href="#">Log out</a>
+                </div>
+            </li>
+        </ul>
 
   </div>
 </nav>
